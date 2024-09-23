ea7cdd3517cbf45022e1b2c7d037ac5b34d093ca
Simon Canil <simon.canil@gmail.com>
2024-09-24 09:55:03 +1000

    fix firebase pkg name for swiftprotobuf

--- Package.swift
+++ Package.swift
@@ -6,19 +6,19 @@ let package = Package(
     platforms: [.iOS(.v13)],
     products: [
         .library(name: "WalletCore", targets: ["WalletCore"]),
-        .library(name: "SwiftProtobuf", targets: ["SwiftProtobuf"])
+        .library(name: "WalletCoreSwiftProtobuf", targets: ["WalletCoreSwiftProtobuf"])
     ],
     dependencies: [],
     targets: [
         .binaryTarget(
             name: "WalletCore",
-            url: "https://github.com/trustwallet/wallet-core/releases/download/4.0.33/WalletCore.xcframework.zip",
-            checksum: "2fb8b833047b9697bba6ade66a9bdeede622b2fe0fb7a9b90cb9edb4651ec866"
+            url: "https://github.com/trustwallet/wallet-core/releases/download/4.1.5/WalletCore.xcframework.zip",
+            checksum: "4b2c1f7386d6d31aa7a7b573bdeb1106dedd3d9989c2b3e8b69579a82aeaab78"
         ),
         .binaryTarget(
-            name: "SwiftProtobuf",
-            url: "https://github.com/trustwallet/wallet-core/releases/download/4.0.33/SwiftProtobuf.xcframework.zip",
-            checksum: "05557735dd607c5a369dc378eb3f299504b880614ef13f136a028ecd320b0e4d"
+            name: "WalletCoreSwiftProtobuf",
+            url: "https://github.com/trustwallet/wallet-core/releases/download/4.1.5/SwiftProtobuf.xcframework.zip",
+            checksum: "9d2faa98ac5d9f107be1f0d0ebaabde051a0828c20f2fbc840049e5ff7e2131c"
         )
     ]
 )
